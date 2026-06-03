9c6eb7ed54b71cf5ae888ca382b82d2a62cf18f8
ShiuKonata <hirerikagami@gmail.com>
2026-06-04 01:00:04 +0800

    Add clip channel to 魁 Kai (+2 insertions)

--- js/data.js
+++ js/data.js
@@ -1709,6 +1709,7 @@ const vtubers = [
       { id: "UCPk2c45wPWSFVgdI7Nry2Xw", label: "槓槓",       keywords: ["魁", "Kai"], typeKeywords: ["歌切", "cover", "歌回", "合唱", "線下連動"] },
       { id: "UCbNA4tD_skq8CQF2aMTx9cA", label: "??????",     keywords: ["魁", "Kai"], typeKeywords: ["歌切", "cover", "歌回", "合唱", "線下連動"] },
       { id: "UCZuULhCMI94q4FgmedxEoSw", label: "Fish??????", keywords: ["魁", "Kai"], typeKeywords: ["歌切", "cover", "歌回", "合唱", "線下連動"] },
+      { id: "UCBdIlFj6vWDxU_m-PyM3aPQ", label: "阿酒精華", typeKeywords: ["cover", "歌ってみた"] },
     ],
 
     videoClipsChannelIds: [
@@ -1717,6 +1718,7 @@ const vtubers = [
       { id: "UCbZrLw_Lmq062fhqdsShr5w", label: "妮妮子nini", keywords: ["魁", "Kai", "小心結天團"] },
       { id: "UCPk2c45wPWSFVgdI7Nry2Xw", label: "槓槓",           keywords: ["魁", "Kai"], excludeKeywords: ["歌切", "cover", "歌回", "合唱", "線下連動"] },
       { id: "UCST8y6kG6QQyAnRwLv8cv_A", label: "羽feather🌙🐋", keywords: ["魁", "Kai", "這很魁以"], excludeKeywords: ["歌切", "cover", "歌回", "合唱", "線下連動"] },
+      { id: "UCBdIlFj6vWDxU_m-PyM3aPQ", label: "阿酒精華", excludeKeywords: ["cover", "歌ってみた"] },
     ],
 
     scheduleVideoId:  "psgIP5rMRTA",
